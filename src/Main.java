b4b3aa4168b3c7e1acaf52b8031ed5d84c974f6c
chapranj <chapranj@sheridancollege.ca>
2023-08-12 22:30:02 -0400

    need to work on the part where the card is drawn from the deck if the current player does not having ( it was causing an exception ) Also need to implement skip and wild cards once the core logic is implemented!

--- src/Main.java
+++ src/Main.java
@@ -3,38 +3,6 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-
-//        ArrayList<Card> cardarr = new ArrayList<>();
-//
-//        Color col= Color.BLUE;
-//        Value val =  Value.FIVE;
-//
-//        Card card = new Card(col,val);
-//        Color col2= Color.RED;
-//        Value val2 =  Value.FOUR;
-//        Card card2 = new Card(col2,val2);
-//
-//        Color col3= Color.YELLOW;
-//        Value val3 =  Value.ONE;
-//        Card card3 = new Card(col3,val3);
-//
-//        cardarr.add(card);
-//        cardarr.add(card2);
-//
-////        System.out.println(cardarr);
-//
-//        Hand pHand = new Hand(cardarr);
-//        pHand.addCardToHand(card3);
-//
-//        Player pranjal = new Player("Pranjal" , pHand);
-//        System.out.println(pranjal.getHand());
-//
-//        System.out.println("all cards: ");
-//        Deck deck = new Deck();
-//        Card[] theDeck = deck.getDeck();
-//
-//        deck.shuffleDeck();
-//        System.out.println(Arrays.toString(theDeck));
         instantiateDeck();
 
     }
@@ -55,7 +23,7 @@ public class Main {
             computerCards.add(deck.getDeck().get(i));
             deck.getDeck().remove(deck.getDeck().get(i));
         }
-//        int i = 14;
+
 
         Card firstCardinPile = deck.getDeck().get(14);
 
@@ -88,14 +56,15 @@ public class Main {
         Player currentPlayer = p;
         Player opponent = c;
 
-        System.out.println(firstCard);
+//        System.out.println(firstCard);
 
         while(!won){
+            System.out.println(firstCard);
             System.out.println("It's "+ currentPlayer.getName()+ "'s "+ "turn!");
-            System.out.println("Your hand: ");
+            System.out.println( currentPlayer.getName()+ "'s "+"hand: ");
 //            System.out.println(p.getHand().seeHand());
             int cardNumber = 1;
-            for (Card card: p.getHand().seeHand()) {
+            for (Card card: currentPlayer.getHand().seeHand()) {
                 System.out.println(String.valueOf(cardNumber) + " " +card );
                 cardIndexHolder.put(cardNumber,card);
                 cardNumber++;
@@ -107,17 +76,15 @@ public class Main {
                 System.out.println(firstCard);
                 System.out.println(cardIndexHolder.get(cardChosen));
                 System.out.println(cardChosen);
+                System.out.println("Card removed from hand "+ currentPlayer.getHand().seeHand().get(cardChosen-1));
+                currentPlayer.getHand().seeHand().remove(cardChosen-1);
 
-
-                System.out.println("Card removed from hand "+ p.getHand().seeHand().get(cardChosen-1));
-                p.getHand().seeHand().remove(cardChosen-1);
-
-                System.out.println("Now hand "+ p.getHand().seeHand());
+                System.out.println("Now "+ currentPlayer.getName()+ "'s" +" hand "+ currentPlayer.getHand().seeHand());
             }
             else{
                 boolean cardFound = false;
-                System.out.println(p.getHand().seeHand().contains(firstCard));
-                for (Card cardInHand: p.getHand().seeHand() ) {
+                System.out.println(currentPlayer.getHand().seeHand().contains(firstCard));
+                for (Card cardInHand: currentPlayer.getHand().seeHand() ) {
                     if ((cardInHand.getValue().equals(firstCard.getValue()) || (cardInHand.getColor().equals(firstCard.getColor())))) {
                         cardFound = true;
                         break;
@@ -131,27 +98,25 @@ public class Main {
                     System.out.println("Necessary card not in your deck...drawing....");
                     for (Card deckCheck: deck.getDeck() ) {
                         if ((deckCheck.getValue().equals(firstCard.getValue())||(deckCheck.getColor().equals(firstCard.getColor())))){
-                            p.getHand().seeHand().add(deckCheck);
+                            currentPlayer.getHand().seeHand().add(deckCheck);
                             deck.getDeck().remove(deckCheck);
                             System.out.println("Added card "+ deckCheck);
                             System.out.println("Now Deck "+ deck.getDeck());
-                            System.out.println("Now Hand "+ p.getHand().seeHand());
+                            System.out.println("Now Hand "+ currentPlayer.getHand().seeHand());
+
                         }
                     }
                 }
-
-                if (currentPlayer.getHand().seeHand().isEmpty()){
-                    won = true;
-                }
-
-                System.out.println("neeche");
-                Player tempPlayer = currentPlayer;
-                currentPlayer = opponent;
-                opponent = tempPlayer;
-                //hard code kar raha hoon sirf check karne ke liye baad mein update karunga
-                firstCard = deck.getDeck().get(15);
-
             }
+            if (currentPlayer.getHand().seeHand().isEmpty()){
+                won = true;
+            }
+            firstCard = cardIndexHolder.get(cardChosen);
+            System.out.println("neeche");
+            Player tempPlayer = currentPlayer;
+            currentPlayer = opponent;
+            opponent = tempPlayer;
+
         }
     }
 
